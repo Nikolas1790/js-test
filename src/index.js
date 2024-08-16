7b3c0dcc2aa4d935e30c602d08d382a27471c9fa
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-16 18:13:58 +0300

    s

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('ssssssss')
+console.log("first")
+console.log("second")
+
+setTimeout(function(){
+    console.log("fourth")
+}, 0)
+
+Promise.resolve().then(()=>{
+    console.log("fifth")
+})
+
+for(let i=0; i<1000; i++){
+    console.log(i)
+}
+
+console.log("third")
